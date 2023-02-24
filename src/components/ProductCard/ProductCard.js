80438c9eaeb06260b7e50a56612a69172005caf5
Erkan <12980@coderacademy.edu.au>
2023-02-24 12:34:34 +1100

    added a ternary operator for items that are on sale, added css to the p tag that's on sale

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -3,24 +3,22 @@ import productList from '../ProductList.js'
 import './productcard.css'
 
 const ProductCard = () => {
-
     return (
         <div className='product-card-container'>
-            {/* <div className='product-card'> */}
-                {productList.map((product, index) => {
-                    return (
+            {productList.map((product, index) => {
+                return (
                     <div className='product-card'>
                         <div className='product-card-item' key={index}>
+                            <p className='for-sale-tag'>{product.isSale ? <p>On Sale!</p> : ''}</p>
                             <img className='product-image' src={product.productImage} alt={product.name}></img>
                             <p>{product.productName}</p>
                             <p>{product.price}</p>
                             <p>{product.type}</p>
-                            <p>{product.isSale}</p>
+
                         </div>
-                    </div>    
-                    )
-                })}
-            {/* </div> */}
+                    </div>
+                )
+            })}
         </div>
     )
 }
